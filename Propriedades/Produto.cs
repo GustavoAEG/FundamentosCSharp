8330a1cce3dcbf17cc4fd49142ef6f056b306aa5
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-09 15:05:29 -0300

    Add project files.

---
+++ Propriedades/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosCSharp.Propriedades
+{
+    public class Produto
+    {
+        public string Nome { get; set; } // Leitura e escrita
+        public decimal Preco { get; private set; } // Somente leitura pública
+        public int Estoque { get; set; }
+
+        public Produto(string nome, decimal preco, int estoque)
+        {
+            Nome = nome;
+            Preco = preco;
+            Estoque = estoque;
+        }
+    }
+}
