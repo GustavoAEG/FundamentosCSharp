57500ae3a32c2dfd5a85479ce1d4642c36791a1e
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-09 15:18:21 -0300

    feat: adicionar classe Colecoes.

---
+++ Coleções/Colecoes.cs
@@ -0,0 +1,14 @@
+﻿namespace FundamentosCSharp.Colecoes;
+
+public class ExemploColecoes
+{
+    public void Executar()
+    {
+        var lista = new List<string> { "A", "B", "C" };
+        var dicionario = new Dictionary<int, string>
+        {
+            { 1, "Um" },
+            { 2, "Dois" }
+        };
+    }
+}
---
+++ Metodos/Metodos.cs
@@ -0,0 +1,12 @@
+﻿namespace FundamentosCSharp.Metodos;
+
+public class Calculadora
+{
+    public void MostrarMensagem(string mensagem) => Console.WriteLine(mensagem);
+
+    public int Somar(int a, int b) => a + b;
+
+    public bool EhPar(int numero) => numero % 2 == 0;
+
+    public List<string> ObterFrutas() => new() { "Maçã", "Banana", "Laranja" };
+}
