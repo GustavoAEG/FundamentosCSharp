399e1ff3c5f8d343cc2f602e79090a9ff245c7d8
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-09 16:52:03 -0300

    feat Operações Assyncronas

--- Metodos/Metodos.cs
+++ Metodos/Metodos.cs
@@ -1,12 +1,55 @@
-﻿namespace FundamentosCSharp.Metodos;
+﻿using FundamentosCSharp.Propriedades;
+
+namespace FundamentosCSharp.Metodos;
 
 public class Calculadora
 {
-    public void MostrarMensagem(string mensagem) => Console.WriteLine(mensagem);
 
-    public int Somar(int a, int b) => a + b;
+    public void MostrarMensagem(string mensagem)
+    {
+        Console.WriteLine(mensagem);
+    }
 
-    public bool EhPar(int numero) => numero % 2 == 0;
+    public int Somar(int a, int b)
+    {
+        int result = a + b;
+        return result;
+    }
 
-    public List<string> ObterFrutas() => new() { "Maçã", "Banana", "Laranja" };
+    public bool EhPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+
+    public Produto CriarProduto(string nome, decimal preco, int estoque)
+    {
+        return new Produto(nome, preco, estoque);
+    }
+
+    public List<string> ObterFrutas()
+    {
+        return new List<string> { "Maçã", "Banana", "Laranja" };
+    }
+
+    public T RetornarPrimeiro<T>(List<T> lista)
+    {
+        return lista.First();
+    }
+
+
+    public async Task<int> SomarAsync(int a, int b)
+    {
+        await Task.Delay(500);
+        return a + b;
+    }
+
+    public static async Task<int> ExecutarSubtracaoAsync(int resultado, int SegundoValor)
+    {
+        await Task.Delay(500);
+
+        int subtresultasync= resultado - SegundoValor;
+
+        return subtresultasync;
+     
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FundamentosCSharp.Colecoes;
+using FundamentosCSharp.Construtores;
+using FundamentosCSharp.Metodos;
+using FundamentosCSharp.Propriedades;
+using FundamentosCSharp.Serializacao;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Iniciando operação demorada...");
+        var tarefa = OperacaoDemoradaAsync();
+
+        Console.WriteLine("Informe Nome:");
+        string entrada = Console.ReadLine();
+        
+        int outroResultado = await CalcularAsync();
+
+        Console.WriteLine($"Resultado da operação: {outroResultado}");
+    }
+
+    static async Task<int> OperacaoDemoradaAsync()
+    {
+        await Task.Delay(60000); 
+        return 42;
+    }
+    static async Task<int> CalcularAsync()
+    {
+        await Task.Delay(2000);
+        return 100;
+    }
+}
