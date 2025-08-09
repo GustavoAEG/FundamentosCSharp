21564a4d353c46d3b1d94ab8dba8b84fdde77653
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-09 15:09:57 -0300

    feat: adicionar classe Cliente com propriedades básicas e formas deiferentes de se criar o construtor.

---
+++ Construtores/Construtores.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FundamentosCSharp.Construtores;
+
+public class Cliente
+{
+    public string Nome { get; }
+    public int Idade { get; }
+
+    public Cliente(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+
+    [JsonConstructor]
+    public Cliente(string nome)
+    {
+        Nome = nome;
+        Idade = 0;
+    }
+}
