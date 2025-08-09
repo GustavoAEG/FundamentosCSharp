1a9da8bf6e3e448998c209c4a65f73c0efc2a25e
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-09 15:19:15 -0300

    feat: adicionar classe Json com propriedades básicas

---
+++ Serialização/ExampleJson.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace FundamentosCSharp.Serializacao;
+
+public class ExemploJson
+{
+    public void Executar()
+    {
+        var pessoa = new { Nome = "João", Idade = 30 };
+        string json = JsonSerializer.Serialize(pessoa);
+        Console.WriteLine(json);
+
+        var obj = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+        Console.WriteLine(obj["Nome"]);
+    }
+}
